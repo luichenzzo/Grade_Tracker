08868862e6e4ba09133dac0125541624f343cd65
odiador <arroa03@gmail.com>
2025-06-02 11:43:46 -0500

    feat: Add initial configuration files and dependencies for Tailwind CSS and Lucide React

--- grade-tracker/src/app/layout.tsx
+++ grade-tracker/src/app/layout.tsx
@@ -9,8 +9,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="es">
-        <body className="bg-gray-100 text-gray-900">{children}</body>
+            <body className="bg-gray-100 text-gray-900" cz-shortcut-listen="true">{children}</body>
         </html>
     )
 }
 //TODO: Check how neccessary is this file and if it can be removed
+// RE: This file is necessary for Next.js to define the root layout of the application.
